769131d5dc2af6ae4f167cf350ee7be4ffa0b01c
Chitransh <94716677+ChitranshS@users.noreply.github.com>
2022-10-05 06:03:42 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var blue_btn = document.getElementById("blue");
+var red_btn = document.getElementById("red");
+var gold_btn = document.getElementById("gold");
+var black_btn = document.getElementById("black");
+var bk = document.getElementById("landing-image");
+var buy_btn = document.getElementById("buy");
+
+blue_btn.addEventListener("click", function onClick(event) {
+  //   console.log("It works");
+  bk.style.backgroundImage = 'url("../resources/images/0_1_6.png")';
+  buy_btn.style.backgroundColor = "#414bb2";
+  buy_btn.style.color = "white";
+});
+red_btn.addEventListener("click", function onClick(event) {
+  //   console.log("It works");
+  bk.style.backgroundImage = 'url("../resources/images/0_1_7.png")';
+  buy_btn.style.backgroundColor = "#a53019";
+  buy_btn.style.color = "white";
+});
+gold_btn.addEventListener("click", function onClick(event) {
+  //   console.log("It works");
+  bk.style.backgroundImage = 'url("../resources/images/0_1_2.png")';
+  buy_btn.style.backgroundColor = "#e4d7b4";
+  buy_btn.style.color = "black";
+});
+black_btn.addEventListener("click", function onClick(event) {
+  console.log("It works");
+  bk.style.backgroundImage = 'url("../resources/images/0_1_5.png")';
+  buy_btn.style.backgroundColor = "#a8b3b6";
+  buy_btn.style.color = "black";
+});
