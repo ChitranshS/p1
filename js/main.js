aee4eec97362d9968092a28fe2c44c974dc05635
Chitransh <94716677+ChitranshS@users.noreply.github.com>
2022-10-05 21:12:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,29 +8,29 @@ var buy_btn = document.getElementById("buy");
 
 blue_btn.addEventListener("click", function onClick(event) {
   //   console.log("It works");
-  landing.style.backgroundImage = 'url("../resources/images/0_1_6.png")';
-  battery.style.backgroundImage = 'url("../resources/images/3_2.png")';
+  landing.style.backgroundImage = 'url("../p1/resources/images/0_1_6.png")';
+  battery.style.backgroundImage = 'url("../p1/resources/images/3_2.png")';
   buy_btn.style.backgroundColor = "#414bb2";
   buy_btn.style.color = "white";
 });
 red_btn.addEventListener("click", function onClick(event) {
   //   console.log("It works");
-  landing.style.backgroundImage = 'url("../resources/images/0_1_7.png")';
-  battery.style.backgroundImage = 'url("../resources/images/3_1.png")';
+  landing.style.backgroundImage = 'url("../p1/resources/images/0_1_7.png")';
+  battery.style.backgroundImage = 'url("../p1/resources/images/3_1.png")';
   buy_btn.style.backgroundColor = "#a53019";
   buy_btn.style.color = "white";
 });
 gold_btn.addEventListener("click", function onClick(event) {
   //   console.log("It works");
-  landing.style.backgroundImage = 'url("../resources/images/0_1_2.png")';
-  battery.style.backgroundImage = 'url("../resources/images/3_4.png")';
+  landing.style.backgroundImage = 'url("../p1/resources/images/0_1_2.png")';
+  battery.style.backgroundImage = 'url("../p1/resources/images/3_4.png")';
   buy_btn.style.backgroundColor = "#e4d7b4";
   buy_btn.style.color = "black";
 });
 black_btn.addEventListener("click", function onClick(event) {
   console.log("It works");
-  landing.style.backgroundImage = 'url("../resources/images/0_1_5.png")';
-  battery.style.backgroundImage = 'url("../resources/images/3_3.png")';
+  landing.style.backgroundImage = 'url("../p1/resources/images/0_1_5.png")';
+  battery.style.backgroundImage = 'url("../p1/resources/images/3_3.png")';
   buy_btn.style.backgroundColor = "#a8b3b6";
   buy_btn.style.color = "black";
 });
